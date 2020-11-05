b8435d5f3304681085fedaaec052a4da66cdcb41
Никита Суханов <nik.152002@mail.ru>
2020-11-05 13:27:34 +0300

    Добавил обработку отправки на почту, залил проект на хостинг

--- src/php/mail.php
+++ src/php/mail.php
@@ -14,5 +14,47 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$to = "nik.152002@mail.ru";
 	$headers = "From: visiter of my portfolio <" + $mailFrom + ">\r\nContent-type: text/html; charset=windows-1251 \r\n";
 	mail($to,$subject,$message,$headers);
+	mail("nik.152002@mail.ru","header","text");	
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<style>
+		body{
+			text-align: center;
+			background: #1F1F20;
+			color: #fff;
+			font-size: 1.5rem;
+			font-family: sans-serif;
+			height: 100vh;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+		}
+		h1{
+			text-shadow: 5px 5px 10px #000;
+		}
+	</style>
+</head>
+<body>
+	<h1>
+	<?php
+$to     = 'nik.152002@mail.ru';
+$subject = "Рад видеть вас на своём сайте!";
+$message = $_POST["userMessage"];
+$email = $_POST["userEmail"];
+$headers = array(
+    'From' => 'nik.152002@mail.ru',
+);
+
+if(mail($to, $subject, $message, $headers)) echo "Письмо было отправлено!";
+else echo "Не удалось отправить письмо! Проверьте соединение с Интернетом!";
+?>
+	</h1>
+</body>
+</html>
