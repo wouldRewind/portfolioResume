3574d720e5f29b85a98af3d6648d0dd69966a5e4
Никита Суханов <nik.152002@mail.ru>
2020-11-28 22:39:15 +0300

    Excluded typedjs cause of repeated bug

--- index.ts
+++ index.ts
@@ -2,15 +2,7 @@
 import "@babel/polyfill";
 import {particleInit} from "./src/js/particle";
 import {listenGithub} from "./src/js/GitHubAPI";
-import Typed from 'typed.js';
 import $ from "jquery";
-
-document.getElementById("myName").addEventListener("animationend",function(){
-	var typed = new Typed('.accent', {
-	typeSpeed: 1,
-	strings: ['Nikita Suhanov'],
-	});
-})
 
 
 
