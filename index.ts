9d267f08bff30b0d3695eb9622b7d51882e9023d
Никита Суханов <nik.152002@mail.ru>
2020-11-30 09:18:38 +0300

    install wow.js via npm and plugged in the project

--- index.ts
+++ index.ts
@@ -3,30 +3,19 @@ import "@babel/polyfill";
 import {particleInit} from "./src/js/particle";
 import {listenGithub} from "./src/js/GitHubAPI";
 import {insertMenu,handleMenu} from "./src/js/menu";
-import $ from "jquery";
+import WOW from "wow.js";
+
 
 
 // перед тем, как загрузятся стили
 document.addEventListener("DOMContentLoaded",function()
 {
-
+	new WOW().init() // инициализирую объект анимации
 	particleInit()
 	listenGithub()
 	insertMenu()
 	handleMenu()
 })
-
-// $(function(){
-// 	$("a[href^='#']").click(function(){
-// 		const blockID : string = $(this).attr("href").slice(1); 
-// 		console.log(blockID);
-		
-// 		document.getElementById(blockID).scrollIntoView({
-// 			behavior:"auto",
-// 			block: "start"
-// 		})
-// 	});
-// });
 
 
 
