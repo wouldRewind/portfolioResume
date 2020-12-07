5f8ac1b280b87d8e1d486d4bd8481c38b801d53b
Suhanov Nick <nik.152002@mail.ru>
2020-12-07 11:34:04 +0300

    Почистил комменты, удалил импорт JQ

--- index.ts
+++ index.ts
@@ -3,30 +3,16 @@ import "@babel/polyfill";
 import {particleInit} from "./src/js/particle";
 import {listenGithub} from "./src/js/GitHubAPI";
 import {insertMenu,handleMenu} from "./src/js/menu";
-import $ from "jquery";
-
 
 // перед тем, как загрузятся стили
 document.addEventListener("DOMContentLoaded",function()
 {
-
 	particleInit()
 	listenGithub()
 	insertMenu()
 	handleMenu()
 })
 
-// $(function(){
-// 	$("a[href^='#']").click(function(){
-// 		const blockID : string = $(this).attr("href").slice(1); 
-// 		console.log(blockID);
-		
-// 		document.getElementById(blockID).scrollIntoView({
-// 			behavior:"auto",
-// 			block: "start"
-// 		})
-// 	});
-// });
 
 
 
